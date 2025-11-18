e33fe8ddf0be36248ac76fe0198df4dc71cc8cbd
LiamViader <liam.viadr@gmail.com>
2025-11-18 21:43:18 +0100

    dasd

--- src/components/projects/modal/animations.ts
+++ src/components/projects/modal/animations.ts
@@ -7,17 +7,17 @@ export const modalContentVariants: Variants = {
     y: 0,
     transition: { duration: 0.4, when: "beforeChildren", staggerChildren: 0.08, ease: "easeOut" },
   },
-  exit: { opacity: 0, y: 0, transition: { duration: 0.2, ease: "easeIn" } },
+  exit: { opacity: 0.5, y: 0, transition: { duration: 0.3, ease: "easeIn" } },
 };
 
 export const modalItemVariants: Variants = {
   hidden: { opacity: 0, y: 0 },
   visible: { opacity: 1, y: 0, transition: { duration: 0.3, ease: "easeOut" } },
-  exit: { opacity: 0, y: 0, transition: { duration: 0.3, ease: "easeIn" } },
+  exit: { opacity: 0, y: 0, transition: { duration: 0.2, ease: "easeIn" } },
 };
 
 export const modalItemVariants2: Variants = {
   hidden: { opacity: 0, y: 0 },
   visible: { opacity: 1, y: 0, transition: { duration: 0.3, ease: "easeOut" } },
-  exit: { opacity: 0, y: 0, transition: { duration: 0.3, ease: "easeIn" } },
+  exit: { opacity: 0, y: 0, transition: { duration: 0.2, ease: "easeIn" } },
 };
