72020db5babeb9bea34aa480165359aedc58cd5e
LiamViader <liam.viadr@gmail.com>
2025-11-20 20:18:36 +0100

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,18 +1,33 @@
+import {NextResponse} from 'next/server';
+import type {NextRequest} from 'next/server';
 import createMiddleware from 'next-intl/middleware';
-import { locales, defaultLocale } from './i18n/routing';
+import {locales, defaultLocale} from './i18n/routing';
 
-export default createMiddleware({
-  locales: locales, 
-
-  defaultLocale: defaultLocale, 
-
-  localePrefix: 'as-needed', 
+const intlMiddleware = createMiddleware({
+  locales,
+  defaultLocale,
+  localePrefix: 'as-needed'
 });
+
+export default function middleware(req: NextRequest) {
+  const host = req.headers.get('host');
+
+  // If comes from the vercel.app domain, redirect to custom domain
+  if (host && host.endsWith('.vercel.app')) {
+    const url = req.nextUrl.clone();
+    url.host = 'liamviader.com'; // my domain
+    url.protocol = 'https';
+    return NextResponse.redirect(url);
+  }
+
+  // Otherwise, proceed with intl middleware
+  return intlMiddleware(req);
+}
 
 export const config = {
   matcher: [
     '/',
-    '/(es|en)/:path*', 
+    '/(es|en)/:path*',
     '/projects/:path*',
     '/contact/:path*',
     '/about/:path*'
