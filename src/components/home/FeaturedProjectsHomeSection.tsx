bbb5283005215f286d93acd7ba2f723000b4d673
LiamViader <liam.viadr@gmail.com>
2025-12-27 02:56:29 +0100

    Update FeaturedProjectsHomeSection.tsx

--- src/components/home/FeaturedProjectsHomeSection.tsx
+++ src/components/home/FeaturedProjectsHomeSection.tsx
@@ -133,7 +133,7 @@ export function FeaturedProjectsHomeSection({
           </Stack>
         </motion.div>
 
-        <Stack size="md">
+        <Stack size="md" className="pt-2 sm:pt-0">
           <FeaturedProjects
             projects={projects}
             introStart={cardsIntro}
