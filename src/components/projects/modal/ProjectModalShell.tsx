6c484e9cfafcde7d6eb2b6242e83d5cd0fffc841
LiamViader <liam.viadr@gmail.com>
2026-01-05 14:54:40 +0100

    Update ProjectModalShell.tsx

--- src/components/projects/modal/ProjectModalShell.tsx
+++ src/components/projects/modal/ProjectModalShell.tsx
@@ -30,7 +30,6 @@ export function ProjectModalShell({
       style={{
         position: "fixed",
         zIndex: 99999,
-        boxShadow: "0 0px 100px rgba(8, 47, 73, 0.5)",
         borderRadius: 24,
         boxSizing: "border-box",
         overflow: "hidden",
