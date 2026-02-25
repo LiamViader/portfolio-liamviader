fd63a5421e824d2b38afb4741d4ed09fe8c9f8c1
LiamViader <viaderliam.dev@gmail.com>
2026-02-25 11:20:37 +0100

    made it easy for google to index icon

---
+++ src/data/projects/list/ButterflyCollector.ts
@@ -0,0 +1,44 @@
+import { ProjectDefinition } from "../types";
+
+export const ButterflyCollector: ProjectDefinition = {
+  slug: "butterfly-collector",
+  date: "2022-05",
+  tags: [
+    "AR",
+    "Mobile",
+    "Unity",
+    "C#"
+  ],
+  media_preview: "/images/projects/butterfly-collector/preview.jpg",
+  links: [
+    {
+      url: "https://github.com/LiamViader/butterfly-collector",
+      label: "GitHub",
+      type: "github",
+      icon: "Github",
+      primaryColor: "rgba(150, 37, 255, 0.7)",
+      secondaryColor: "rgba(168, 85, 247, 1)",
+    },
+  ],
+  detailed_media: [
+
+  ],
+  categories: ["Game"],
+  is_featured: true,
+  translations: {
+    en: {
+      title: "Butterfly Collector",
+      short_description:
+        ".",
+      full_description: `.`,
+      role: "Creator, Designer, and Developer of the Full Videogame",
+    },
+    es: {
+      title: "Butterfly Collector",
+      short_description:
+        ".",
+      full_description: `.`,
+      role: "Creator, Designer, and Developer of the Full Videogame",
+    },
+  },
+};
