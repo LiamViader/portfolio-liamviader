907da5d3ba31621be43d5389faff2c3f0be50945
LiamViader <viaderliam.dev@gmail.com>
2026-03-05 22:47:41 +0100

    dpr increased again

--- src/components/projects/ProjectSceneCanvas.tsx
+++ src/components/projects/ProjectSceneCanvas.tsx
@@ -15,7 +15,7 @@ export function ProjectSceneCanvas({ category }: ProjectSceneCanvasProps) {
     <div className="fixed top-0 left-0 w-full h-[100lvh] z-0" style={{ paddingRight: "var(--scrollbar-gap)" }}>
       <Canvas
         camera={{ position: [0, 0, 0], fov: 75 }}
-        dpr={1}
+        dpr={[1, 1.5]}
       >
         <ProjectSceneContent category={category} />
       </Canvas>
